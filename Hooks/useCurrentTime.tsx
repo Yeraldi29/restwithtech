087c312d33117fb06f56d9df22a5b9ab065e6470
Yeraldi29 <yeraldimujicarosales@gmail.com>
2022-12-10 21:12:59 -0400

    create current time hook and added to each comment

---
+++ Hooks/useCurrentTime.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react"
+import { Timestamp } from "firebase/firestore";
+import { useTranslation } from "next-i18next";
+
+export const useCurrentTime = (create_at:Timestamp) => {
+    const [ timeString, setTimeString ] = useState("")
+
+    const { t } = useTranslation("common")
+    
+    useEffect(()=>{
+        const time = new Date().getTime()
+
+        const currentSeconds = Math.floor((time - create_at.toMillis() )/1000)
+        const currentMinutes = Math.floor((time - create_at.toMillis() )/60000)
+        const currentHours = Math.floor((time - create_at.toMillis() )/3600000)
+
+        if(currentSeconds < 60){
+            setTimeString(t("time.seconds", {currentSeconds}))
+        }else if(currentMinutes < 60){
+            if(currentMinutes === 1){
+                setTimeString(t("time.minute",{currentMinutes}))
+            }else{
+              setTimeString(t("time.minutes",{currentMinutes}))
+            }
+        }else if(currentHours < 72){
+            if(currentHours === 1){
+                setTimeString(t("time.hour", {currentHours}))
+              }else if(currentHours < 24){
+                setTimeString(t("time.hours", {currentHours}))
+              }else if(currentHours < 48){
+                setTimeString(t("time.yesterday"))
+              }else if(currentHours < 72){
+                setTimeString(t("time.beforeYesterday"))
+              }
+        }else{
+            setTimeString((new Date(create_at.toDate()).toLocaleDateString()))
+        }
+
+    },[create_at])
+    
+    return { timeString }
+}
--- components/newInformation/comments/comment/HeaderComm.tsx
+++ components/newInformation/comments/comment/HeaderComm.tsx
@@ -1,4 +1,7 @@
 import { Timestamp } from "firebase/firestore"
+import { useTranslation } from "next-i18next"
+import { useState,useEffect } from "react"
+import { useCurrentTime } from "../../../../Hooks/useCurrentTime"
 import { useAuthValue } from "../../../../store/AuthContext"
 import ImageProfile from "../../../profile/ImageProfile"
 
@@ -9,8 +12,11 @@ interface HeaderCommProps {
 }
 
 const HeaderComm = ({username,imageProfile, create_at}:HeaderCommProps) => {
+  
     const { currentUser } = useAuthValue()
-    
+
+    const { timeString } = useCurrentTime(create_at )
+
   return (
     <div className={`flex items-center justify-between w-full border-4 border-b-0  p-2  rounded-xl ${username === currentUser?.displayName ? "border-DarkBlueGray bg-Blue-Gray" :"border-Blue-Gray bg-DarkBlueGray"}`}>
        <div className="flex items-center space-x-2">
@@ -18,7 +24,7 @@ const HeaderComm = ({username,imageProfile, create_at}:HeaderCommProps) => {
          <h3 className="text-lg md:text-xl xl:text-2xl">{username}</h3>
          </div>
         <h5 className={`text-base font-semibold xl:text-lg  ${username === currentUser?.displayName ? "text-BlueDarker" : "text-BabyBlueEyes"}`}>
-          {new Date(create_at.toDate()).toLocaleDateString()}
+          {timeString}
         </h5>
     </div>
   )
