e0a70736174540c3ddf1d0a54f68d0bf8f649901
Yeraldi29 <yeraldimujicarosales@gmail.com>
2023-01-13 15:17:41 -0400

    Trying to fix translation in dynamic page 2

--- pages/tech/[newPost].tsx
+++ pages/tech/[newPost].tsx
@@ -69,9 +69,8 @@ export const getStaticProps = async ({ locale }:{locale:string}) => ({
 })
 
 export const getStaticPaths = async ({ locales }:{locales:Array<string>}) => {
-  const data = await getDocs(query(collection(db,"news"),where("category","==","tech"))); 
   
-  const paths = data.docs.flatMap(item => {
+  const paths = await (await getDocs(query(collection(db,"news"),where("category","==","tech")))).docs.flatMap(item => {
     return locales.map(locale => {
       return {
         params: { newPost : `${item.data().mainTitle}`},
