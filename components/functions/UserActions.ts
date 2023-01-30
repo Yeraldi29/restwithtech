34524a4e62fdf3ccbb890c2ada79407641585353
Yeraldi29 <yeraldimujicarosales@gmail.com>
2023-01-30 10:27:13 -0400

    fixed redirections in signin and login users

--- components/functions/UserActions.ts
+++ components/functions/UserActions.ts
@@ -1,59 +1,88 @@
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, signInWithEmailAndPassword} from "firebase/auth"
-import type { NextRouter } from "next/router"
-import type { TFunction } from "next-i18next"
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import type { NextRouter } from "next/router";
+import type { TFunction } from "next-i18next";
 
-interface PropsUserActions{
-    validation: boolean
-    formValues:{
-        email: string
-        password: string
-    }
-    handleOther: (err:string|boolean) => void
-    handleTimeActive: (time: boolean) => void
-    title: string
-    Router: NextRouter
-    t: TFunction
+interface PropsUserActions {
+  validation: boolean;
+  formValues: {
+    email: string;
+    password: string;
+  };
+  handleOther: (err: string | boolean) => void;
+  handleTimeActive: (time: boolean) => void;
+  title: string;
+  Router: NextRouter;
+  t: TFunction;
 }
 
-const UserActions = ({validation, formValues, handleOther, handleTimeActive, title,Router, t}: PropsUserActions) =>{
-    const auth = getAuth()
-    const locale = Router.locale
-    if(validation){
-        switch(title){
-            case (t("signIn.signin")):{
-                createUserWithEmailAndPassword(auth, formValues.email, formValues.password ).then(userCredentials=>{
-                sendEmailVerification(userCredentials.user).then(()=>{
-                    Router.push(`${locale === "es/" ? locale : "" } sign-in/verification`)
-                        handleTimeActive(true)
-                })
-            }).catch(err => {
-                handleOther(err.message)
-                console.log("🚀 ~ file: FormInputs.tsx ~ line 64 ~ createUserWithEmailAndPassword ~ err.message", err.message)
-            })  
-            break
+const UserActions = ({
+  validation,
+  formValues,
+  handleOther,
+  handleTimeActive,
+  title,
+  Router,
+  t,
+}: PropsUserActions) => {
+  const auth = getAuth();
+  const locale = Router.locale;
+  if (validation) {
+    switch (title) {
+      case t("signIn.signin"): {
+        createUserWithEmailAndPassword(
+          auth,
+          formValues.email,
+          formValues.password
+        )
+          .then((userCredentials) => {
+            sendEmailVerification(userCredentials.user).then(() => {
+              Router.push(
+                `${locale === "es/" ? locale : ""} sign-in/verification`
+              );
+              handleTimeActive(true);
+            });
+          })
+          .catch((err) => {
+            handleOther(err.message);
+            console.log(
+              "🚀 ~ file: FormInputs.tsx ~ line 64 ~ createUserWithEmailAndPassword ~ err.message",
+              err.message
+            );
+          });
+        break;
+      }
+      case t("logIn.login"): {
+        signInWithEmailAndPassword(auth, formValues.email, formValues.password)
+          .then((userCredantials) => {
+            const user = userCredantials.user;
+            if (user.emailVerified) {
+              if (user.displayName === null && user.photoURL === null) {
+                Router.push(
+                  `${locale === "es/" ? locale : ""}log-in/completeProfile`
+                );
+              } else {
+                Router.push("/");
+              }
+            } else {
+              handleOther(user.emailVerified);
             }
-            case (t("logIn.login")):{
-                signInWithEmailAndPassword(auth,formValues.email,formValues.password)
-                .then(userCredantials => {
-                    const user = userCredantials.user
-                    if(user.emailVerified){
-                        if(user.displayName === null && user.photoURL === null){
-                            Router.push(`${locale === "es/" ? locale : "" }log-in/completeProfile`)
-                        }else{
-                            Router.push("/")
-                        }
-                    }else{
-                        handleOther(user.emailVerified)
-                    }
-                })
-                .catch(err => {
-                    handleOther(err.message)
-                    console.log("🚀 ~ file: UserActions.ts ~ line 38 ~ UserActions ~ err.message", err.message)
-                })
-                break
-            }
-        }    
+          })
+          .catch((err) => {
+            handleOther(err.message);
+            console.log(
+              "🚀 ~ file: UserActions.ts ~ line 38 ~ UserActions ~ err.message",
+              err.message
+            );
+          });
+        break;
+      }
     }
-}
+  }
+};
 
-export default UserActions
+export default UserActions;
--- pages/sign-in/verification/index.tsx
+++ pages/sign-in/verification/index.tsx
@@ -1,113 +1,142 @@
-import Head from "next/head"
-import Router from "next/router"
-import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-import { NextPageWithLayout } from "../../_app"
-import { useState, useEffect } from "react"
-import { useTranslation } from "next-i18next"
-import { useAuthValue } from "../../../store/AuthContext"
-import { auth } from "../../../firebase"
-import { sendEmailVerification, signOut} from "firebase/auth"
+import Head from "next/head";
+import Router from "next/router";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { NextPageWithLayout } from "../../_app";
+import { useState, useEffect } from "react";
+import { useTranslation } from "next-i18next";
+import { useAuthValue } from "../../../store/AuthContext";
+import { auth } from "../../../firebase";
+import { sendEmailVerification, signOut } from "firebase/auth";
 
 const Verification: NextPageWithLayout = () => {
-   const {t} = useTranslation("signIn_logIn")
-   const [animation, setAnimation] = useState(false)
-   const [render, setRender] = useState(false)
-   const [time, setTime] = useState(60)
+  const { t } = useTranslation("signIn_logIn");
+  const [animation, setAnimation] = useState(false);
+  const [render, setRender] = useState(false);
+  const [time, setTime] = useState(60);
 
-   const {currentUser, timeActive, handleTimeActive} = useAuthValue()
+  const { currentUser, timeActive, handleTimeActive } = useAuthValue();
 
-   const resedEmailVerification = () => {
-    
-    if(auth.currentUser){
-      sendEmailVerification(auth.currentUser).then( () => {
-        handleTimeActive(true)
-      }).catch(err => {
-        console.log("🚀 ~ file: index.tsx ~ line 25 ~ sendEmailVerification ~ err.message", err.message)
-      })
+  const resedEmailVerification = () => {
+    if (auth.currentUser) {
+      sendEmailVerification(auth.currentUser)
+        .then(() => {
+          handleTimeActive(true);
+        })
+        .catch((err) => {
+          console.log(
+            "🚀 ~ file: index.tsx ~ line 25 ~ sendEmailVerification ~ err.message",
+            err.message
+          );
+        });
+    }
+  };
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | undefined;
+
+    if (timeActive && time !== 0) {
+      interval = setInterval(() => {
+        setTime((time) => time - 1);
+      }, 1000);
+    } else if (time === 0) {
+      handleTimeActive(false);
+      setTime(60);
+      clearInterval(interval);
     }
 
-   }
+    return () => clearInterval(interval);
+  }, [timeActive, time]);
 
-   useEffect(()=>{
-    let interval: NodeJS.Timeout | undefined
+  useEffect(() => {
+    const interval = setInterval(() => {
+      currentUser
+        ?.reload()
+        .then(() => {
+          if (currentUser?.emailVerified) {
+            clearInterval(interval);
+            signOut(auth)
+              .then(() => {
+                Router.push("/log-in");
+              })
+              .catch((err) => {
+                console.log(
+                  "🚀 ~ file: index.tsx ~ line 54 ~ signOut ~ err",
+                  err.message
+                );
+              });
+          }
+        })
+        .catch((err) => {
+          console.log(
+            "🚀 ~ file: index.tsx ~ line 60 ~ currentUser?.reload ~ err.message",
+            err.message
+          );
+        });
+    }, 1000);
 
-    if(timeActive && time !== 0){
-      interval = setInterval(()=>{
-        setTime(time => time - 1)
-      },1000)
-    }else if( time === 0){
-      handleTimeActive(false)
-      setTime(60)
-      clearInterval(interval)
+    if (!currentUser && !timeActive) {
+      Router.push("/sign-in");
+      setRender(false);
+    } else {
+      setRender(true);
     }
-    
-    return () => clearInterval(interval)
-   },[timeActive, time])
+  }, [currentUser]);
 
-   useEffect(()=>{
-    const interval = setInterval(()=>{
-      currentUser?.reload().then(()=>{
-        if(currentUser?.emailVerified){
-          clearInterval(interval)
-          signOut(auth).then(()=>{
-            Router.push("/log-in")
-          }).catch(err => {
-            console.log("🚀 ~ file: index.tsx ~ line 54 ~ signOut ~ err", err.message)
-          })
-        }
-      }).catch(err=>{
-        console.log("🚀 ~ file: index.tsx ~ line 60 ~ currentUser?.reload ~ err.message", err.message)
-      })
-    },1000)
-    
-    if(!currentUser){
-      Router.push("/sign-in")
-      setRender(false)
-    }else{
-      setRender(true)
-    }
-    
-   },[currentUser])
-  
   return (
     <>
-    <Head>
-      <title>{t("verification.titlePage")}</title>
-      <link rel="icon" href="/icon.png" />
-    </Head>
+      <Head>
+        <title>{t("verification.titlePage")}</title>
+        <link rel="icon" href="/icon.png" />
+      </Head>
 
-    { render && (
-      <div className=" bg-DarkBlueGray flex flex-col items-center space-y-5 mx-auto w-[21rem] -rotate-1 rounded-2xl py-10 px-6 mt-24  md:w-[30rem] xl:mt-36">
-      <h1 className=" text-xl text-center">{t("verification.title")}</h1>
-      <p className="text-center">
-        <strong>{t("verification.message")}</strong>
-        <span className=" text-Lavender-Blue">{currentUser?.email}</span>
-      </p>
-      <p>{t("verification.suggestion")}</p>
-      
-      <button className={`${animation && " animate-wiggle "} bg-Lavender-Blue rounded-xl mt-6 p-2 -rotate-12 text-Blue-Gray lg:hover:opacity-50 active:bg-white cursor-pointer`} 
-      onClick={()=>{
-      setAnimation(true)
-      resedEmailVerification()
-      }}
-      onAnimationEnd={()=>{setAnimation(false)}} 
-      disabled={timeActive}
-      >
-        <p>{t("verification.resend")}<span className={`${timeActive && "ml-2 text-white  bg-DarkBlueGray rounded-md p-1"}`}>{timeActive && time}</span></p>
-      </button>      
-    </div>
-      )
-    }
+      {render && (
+        <div className=" bg-DarkBlueGray flex flex-col items-center space-y-5 mx-auto w-[21rem] -rotate-1 rounded-2xl py-10 px-6 mt-24  md:w-[30rem] xl:mt-36">
+          <h1 className=" text-xl text-center">{t("verification.title")}</h1>
+          <p className="text-center">
+            <strong>{t("verification.message")}</strong>
+            <span className=" text-Lavender-Blue">{currentUser?.email}</span>
+          </p>
+          <p>{t("verification.suggestion")}</p>
+
+          <button
+            className={`${
+              animation && " animate-wiggle "
+            } bg-Lavender-Blue rounded-xl mt-6 p-2 -rotate-12 text-Blue-Gray lg:hover:opacity-50 active:bg-white cursor-pointer`}
+            onClick={() => {
+              setAnimation(true);
+              resedEmailVerification();
+            }}
+            onAnimationEnd={() => {
+              setAnimation(false);
+            }}
+            disabled={timeActive}
+          >
+            <p>
+              {t("verification.resend")}
+              <span
+                className={`${
+                  timeActive &&
+                  "ml-2 text-white  bg-DarkBlueGray rounded-md p-1"
+                }`}
+              >
+                {timeActive && time}
+              </span>
+            </p>
+          </button>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export const getStaticProps = async ({ locale }:{locale:string}) => {
+export const getStaticProps = async ({ locale }: { locale: string }) => {
   return {
-    props: {...await serverSideTranslations(locale, ['signIn_logIn','header']),}
-  }
-}
+    props: {
+      ...(await serverSideTranslations(locale, ["signIn_logIn", "header"])),
+    },
+  };
+};
 
-Verification.getLayout = (page) => page
+Verification.getLayout = (page) => page;
 
-export default Verification
+export default Verification;
