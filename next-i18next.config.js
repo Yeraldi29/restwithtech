6579fd39a3d4f7143818724497cb0ed90aa03ddc
Yeraldi29 <yeraldimujicarosales@gmail.com>
2023-01-31 18:42:03 -0400

    added localePath in next-i18next

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,5 +2,9 @@ module.exports = {
     i18n:{
         defaultLocale: 'en',
         locales: ['en','es'],
-    }
+    },
+    localePath:
+    typeof window === 'undefined'
+      ? require('path').resolve('./public/locales')
+      : '/locales',
 }
